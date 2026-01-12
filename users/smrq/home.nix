9158ee32dac26a35d7d8385821fd28e219def001
Greg Smith <smrq@smrq.net>
2026-01-12 17:47:18 -0600

    Git config

--- users/smrq/home.nix
+++ users/smrq/home.nix
@@ -38,6 +38,7 @@ in
       settings.user.signingkey = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIN3ORdArlT3Dya0q/lgJ4pnrN84oeglS9D7omApOGtMX";
 
       settings.init.defaultBranch = "main";
+      settings.push.autoSetupRemote = true;
       settings.alias = {
         amend = "commit --amend --no-edit";
         l = "log --oneline --graph --decorate --all";
