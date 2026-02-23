44dba14c7a25b896c16d6c87444f0c4be33df963
Greg Smith <smrq@smrq.net>
2026-02-23 11:13:28 -0600

    switch to networkmanager

--- nixos/features/lan/wifi.nix
+++ nixos/features/lan/wifi.nix
@@ -1,10 +1,62 @@
 {
-  networking.wireless = {
+  config,
+  username,
+  ...
+}: {
+  networking.networkmanager = {
     enable = true;
-    networks = {
-      "Hello Neighbor" = {
-        pskRaw = "1f7b7fe3777b373604cc2296105d338b05529aa5bad9b5d8bcdf902122905c17";
+    ensureProfiles = {
+      environmentFiles = [
+        config.sops.secrets."wifi/home/ssid".path
+        config.sops.secrets."wifi/home/psk".path
+      ];
+
+      profiles = {
+        "home" = {
+          connection = {
+            id = "home";
+            type = "wifi";
+            autoconnect = true;
+          };
+          wifi = {
+            mode = "infrastructure";
+            ssid = "$HOME_SSID";
+          };
+          wifi-security = {
+            auth-alg = "open";
+            key-mgmt = "wpa-psk";
+            psk = "$HOME_PSK";
+          };
+          ipv4 = {
+            method = "auto";
+          };
+          ipv6 = {
+            method = "auto";
+            addr-gen-mode = "stable-privacy";
+          };
+        };
       };
     };
   };
+
+  networking.nameservers = [
+    "1.1.1.1"
+    "1.0.0.1"
+    "8.8.8.8"
+    "8.8.4.4"
+  ];
+
+  sops = {
+    secrets = {
+      "wifi/home/ssid" = { };
+      "wifi/home/psk" = { };
+    };
+  };
+
+  users.users.${username} = {
+    extraGroups = [
+      "networkmanager"
+    ];
+  };
 }
+ 
