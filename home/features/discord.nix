39168feda62dca2e9bd4e7ba2e4bb410763e6385
Greg Smith <smrq@smrq.net>
2026-06-06 15:20:28 -0500

    discord being a shit

--- home/features/discord.nix
+++ home/features/discord.nix
@@ -2,20 +2,5 @@
   inputs,
   ...
 }: {
-  imports = [ inputs.nixcord.homeModules.nixcord ];
-
-  programs.nixcord = {
-    enable = true;
-    discord.vencord.enable = true;
-    discord.autoscroll.enable = true;
-    quickCss = ''
-      .winButtons_c38106 {
-        display: none;
-      }
-    '';
-    config = {
-      useQuickCss = true;
-      frameless = true;
-    };
-  };
+  programs.discord.enable = true;
 }
