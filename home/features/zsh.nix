9ace414f6c7b77f48fc0ebde602b87aa30c8500f
Greg Smith <smrq@smrq.net>
2026-06-08 17:16:58 -0500

    pskill

--- home/features/zsh.nix
+++ home/features/zsh.nix
@@ -15,6 +15,17 @@
       nixboot = "nixos-rebuild boot --sudo --flake ${config.home.homeDirectory}/git/nix-config#${hostname}";
       nixgc = "nix-collect-garbage -d";
     };
+    initContent = ''
+      pskill() {
+        local pid=$(ps aux | grep -v grep | grep "$1" | awk '{print $2}' | head -n 1)
+        if [[ -n "$pid" ]]; then
+          kill $pid
+        else
+          echo "No process found matching '$1'."
+          return 1
+        fi
+      }
+    '';
     history.size = 10000;
   };
 }
