b32f40f923aea12a4a9e663c0c5009b0fdc4d0c7
Greg Smith <smrq@smrq.net>
2026-06-08 19:15:42 -0500

    sign builds

--- nixos/features/system/nix.nix
+++ nixos/features/system/nix.nix
@@ -1,4 +1,7 @@
 {
+  config,
+  ...
+}: {
   nixpkgs.config.allowUnfree = true;
   nix.settings = {
     download-buffer-size = 134217728;
@@ -6,10 +9,19 @@
       "nix-command"
       "flakes"
     ];
+    secret-key-files = [
+      config.sops.secrets."nix/binary_cache_key".path
+    ];
+    trusted-public-keys = [
+      "remote-builder-key:YenZJr7mCInsyNSC94E+wKu0V7f976SZdk1fAA5uPnQ="
+    ];
   };
   nix.optimise.automatic = true;
   nix.gc = {
     automatic = true;
     options = "--delete-older-than 30d";
+  };
+  sops.secrets = {
+    "nix/binary_cache_key" = { };
   };
 }
