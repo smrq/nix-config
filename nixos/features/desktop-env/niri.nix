e5d4afffe7282b49ac07380d1efcfa26cc4e045b
Greg Smith <smrq@smrq.net>
2026-06-26 13:34:32 -0500

    add force render niri patch

--- nixos/features/desktop-env/niri.nix
+++ nixos/features/desktop-env/niri.nix
@@ -17,4 +17,19 @@
       };
     };
   };
+
+  # Enable force rendering background windows
+  # https://github.com/niri-wm/niri/pull/2609#issuecomment-4642769618
+  nixpkgs.overlays = [
+    (final: prev: {
+      niri = prev.niri.overrideAttrs (oldAttrs: {
+        patches = (oldAttrs.patches or []) ++ [
+          (prev.fetchpatch {
+            url = "https://github.com/user-attachments/files/28681567/niri-force-render.patch";
+            sha256 = "sha256-1uPF2m5qDtyN9udUJDRtgeZQbktA2VHlo4BB+/l3NGU=";
+          })
+        ];
+      });
+    })
+  ];
 }
